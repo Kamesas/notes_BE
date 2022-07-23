f3d2eaec2cb967ab7e4ae3c4c276b14f5c4d54b4
Kamesas <semakaleksandr2014@gmail.com>
2022-07-23 14:43:05 +0300

    remove localhost from listening port

--- index.js
+++ index.js
@@ -72,5 +72,5 @@ app.patch(
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+  console.log("server listening");
 });
